363bc5da06b6f9fa7a6ad8deddd391e23ffe3283
Thomas Brazier <thomas.brazier@univ-rennes1.fr>
2022-04-01 09:31:32 +0200

    Modified for Slurm Cluster

--- bash.sh
+++ bash.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+. /local/env/envsnakemake-6.0.5.sh
+. /local/env/envsingularity-3.8.5.sh
+. /local/env/envconda.sh
 snakemake -s Snakefile -p -j 16 --config dataset=${1} --configfile data/${1}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete
