5dccee70b322a4ffc6008cf06aa39ec7f968677f
Thomas Brazier <thomas.brazier@univ-rennes1.fr>
2022-04-10 10:19:37 +0200

    Minor changes

--- bash.sh
+++ bash.sh
@@ -7,4 +7,6 @@
 . /local/env/envsnakemake-6.0.5.sh
 . /local/env/envsingularity-3.8.5.sh
 . /local/env/envconda.sh
-snakemake -s Snakefile -p -j 16 --config dataset=${1} --configfile data/${1}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete
+snakemake -s Snakefile -p -j 16 --config dataset=${1} interval.bpen=5 --configfile data/${1}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete -F
+snakemake -s Snakefile -p -j 16 --config dataset=${1} interval.bpen=15 --configfile data/${1}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete
+snakemake -s Snakefile -p -j 16 --config dataset=${1} interval.bpen=25 --configfile data/${1}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete
