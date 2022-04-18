8a5752fad95dd5937e766498ceec2b7f4938a491
Thomas Brazier <thomas.brazier@univ-rennes1.fr>
2022-04-18 18:32:56 +0200

    Rethink the smcpp part

--- bash.sh
+++ bash.sh
@@ -8,5 +8,5 @@
 . /local/env/envsingularity-3.8.5.sh
 . /local/env/envconda.sh
 snakemake -s Snakefile -p -j 16 --configfile data/${1}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete --config dataset=${1} bpen=5 
-snakemake -s Snakefile -p -j 16 --configfile data/${1}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete --config dataset=${1} bpen=15 
-snakemake -s Snakefile -p -j 16 --configfile data/${1}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete --config dataset=${1} bpen=25 
+#snakemake -s Snakefile -p -j 16 --configfile data/${1}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete --config dataset=${1} bpen=15 
+#snakemake -s Snakefile -p -j 16 --configfile data/${1}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete --config dataset=${1} bpen=25 
