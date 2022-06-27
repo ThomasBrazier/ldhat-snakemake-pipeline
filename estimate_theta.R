ca5d3219e46248aaabc10ee30e6344cd75246259
Thomas Brazier <thomas.brazier@univ-rennes1.fr>
2022-06-27 16:34:56 +0200

    Fix for Theta

--- estimate_theta.R
+++ estimate_theta.R
@@ -15,12 +15,13 @@ require(vcfR)
 require(pegas)
 
 # vcf_file = "data/Oryza_sativa_McCouch2016/Oryza_sativa_McCouch2016_test.vcf.gz"
-vcf_file = list.files(path = wdirpop, pattern = paste(set, ".pop.vcf.gz$", sep = ""), full.names = TRUE)
+vcf_file = list.files(path = wdirpop, pattern = paste(".pop.vcf.gz$", sep = ""), full.names = TRUE)
 
 cat("Loading vcf file", vcf_file,".\n")
 vcf = read.vcfR(vcf_file, verbose = FALSE, convertNA = FALSE)
 print(vcf)
 
+cat("Estimating Theta Watterson per site\n")
 # Estimate theta Watterson per site
 # Count how many segregating sites per position
 res = apply(vcf@gt, 1, function(x) {x == "0/1" | x =="1/0" | x == "0|1" | x =="1|0"})
@@ -29,9 +30,8 @@ s = apply(res, 2, sum)
 # s = dim(vcf@gt)[1] # Number of segregating sites at a position
 n = dim(vcf@gt)[2] # Number of sequences
 th = theta.s(s, n, variance = FALSE)
-th
 
-# Compute the mean Theta and its CI
+cat("Compute the mean Theta and 95% CI\n")
 # Bootstrap
 nboot = 1000
 boot = numeric(nboot)
@@ -43,7 +43,8 @@ df = data.frame(mean.theta = mean(boot, na.rm = TRUE),
                 upper.theta = quantile(boot, 0.975, na.rm = TRUE))
 
 # Save results
-write.table(df, file = paste(wdirpop, "theta.txt", sep = ""),
+cat("Saving\n")
+write.table(df, file = paste(wdirpop, "/theta.txt", sep = ""),
             quote = F, col.names = T, row.names = F, sep = "\t")
 
 
