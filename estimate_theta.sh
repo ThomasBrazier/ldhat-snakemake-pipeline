44cc8195219e7c4d9a8d9b094a9d39a246cb6f25
Thomas Brazier <thomas.brazier@univ-rennes1.fr>
2022-06-27 21:30:32 +0200

    Fix theta path

---
+++ estimate_theta.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --mail-user=thomas.brazier@univ-rennes1.fr
+#SBATCH --mail-type=all
+#SBATCH --time=12-60:00:00
+#SBATCH --job-name=LDmap
+
+. /local/env/envsnakemake-6.0.5.sh
+. /local/env/envsingularity-3.8.5.sh
+. /local/env/envconda.sh
+snakemake -s estimate_theta.snake -p -j 16 --configfile data/${1}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete
