6f4fa707f37c0a2ba2acc1f22ff2525c27d58a48
Thomas Brazier <brazier.thomas@gmail.com>
2022-06-27 16:46:10 +0200

    Fix Theta

--- estimate_theta.R
+++ estimate_theta.R
@@ -15,23 +15,25 @@ require(vcfR)
 require(pegas)
 
 # vcf_file = "data/Oryza_sativa_McCouch2016/Oryza_sativa_McCouch2016_test.vcf.gz"
-vcf_file = list.files(path = wdirpop, pattern = paste(".pop.vcf.gz$", sep = ""), full.names = TRUE)
+vcf_file = list.files(path = wdirpop, pattern = paste(set, ".pop.vcf.gz$", sep = ""), full.names = TRUE)
 
 cat("Loading vcf file", vcf_file,".\n")
 vcf = read.vcfR(vcf_file, verbose = FALSE, convertNA = FALSE)
 print(vcf)
 
-cat("Estimating Theta Watterson per site\n")
 # Estimate theta Watterson per site
 # Count how many segregating sites per position
-res = apply(vcf@gt, 1, function(x) {x == "0/1" | x =="1/0" | x == "0|1" | x =="1|0"})
-s = apply(res, 2, sum)
+geno = vcf@gt[,-1]
+pos = matrix(unlist(apply(geno, c(1,2), function(x) {grepl("0/1", x) | grepl("1/0", x)})),
+                nrow = dim(geno)[1], ncol = dim(geno)[2])
+s = apply(pos, 2, sum)
 # theta per site
 # s = dim(vcf@gt)[1] # Number of segregating sites at a position
 n = dim(vcf@gt)[2] # Number of sequences
 th = theta.s(s, n, variance = FALSE)
+th
 
-cat("Compute the mean Theta and 95% CI\n")
+# Compute the mean Theta and its CI
 # Bootstrap
 nboot = 1000
 boot = numeric(nboot)
@@ -43,8 +45,7 @@ df = data.frame(mean.theta = mean(boot, na.rm = TRUE),
                 upper.theta = quantile(boot, 0.975, na.rm = TRUE))
 
 # Save results
-cat("Saving\n")
-write.table(df, file = paste(wdirpop, "/theta.txt", sep = ""),
+write.table(df, file = paste(wdirpop, "theta.txt", sep = ""),
             quote = F, col.names = T, row.names = F, sep = "\t")
 
 
