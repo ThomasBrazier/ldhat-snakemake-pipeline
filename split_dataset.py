abaa3664e0f38e1171115d821ef3b4b644d0dd73
Thomas Brazier <thomas.brazier@univ-rennes1.fr>
2022-07-08 10:10:34 +0200

    Working version of large samples

--- split_dataset.py
+++ split_dataset.py
@@ -12,6 +12,7 @@ Where:
     size:     is the number of SNPs to keep
     overlap:  is the number of overlapping SNPs
 
+For example, 2,000 total SNPs, with 200 SNPs at the beginning/end shared with previous/next chunk
 """
 
 import sys
@@ -34,12 +35,11 @@ def get_overlapped_chunks(text, chunksize, overlapsize):
     return [ lines[a:a+chunksize] for a in range(0,len(lines), chunksize-overlapsize)]
 
 lines = open(inputfile, 'r').readlines() # text as single line
-#lines = str.split(text)
 
 chunks = get_overlapped_chunks(lines, size, overlap)
 
 length = len(chunks) 
-filenames=[f"{outputfile}/batch_{x+1}.vcf" for x in range(length) ]
+filenames=[f"{outputfile}/batch_{x+1}.pos" for x in range(length) ]
 
 #write output
 for chunk, files in zip(chunks, filenames):
