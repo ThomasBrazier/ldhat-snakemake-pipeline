4d946ded198978cbf040142825e9fb91a8e7cddc
tbrazier <brazier.thomas@gmail.com>
2023-02-21 09:45:55 +0100

    Launcher script for MCMC

--- bash_mcmcchains.sh
+++ bash_mcmcchains.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
+#SBATCH --mail-user=thomas.brazier@univ-rennes1.fr
+#SBATCH --mail-type=all
+#SBATCH --mem=60GB
+#SBATCH --cpus-per-task=4
+#SBATCH --time=25-60:00:00
+#SBATCH --job-name=ldhatMCMC
 
+# Load env
+. /local/env/envsnakemake-6.0.5.sh
+. /local/env/envsingularity-3.8.5.sh
+. /local/env/envconda.sh
+
+dataset=${1}
+ncores=16
+
+echo "Run pipeline"
+snakemake -s data_mcmcchains.snake -p -j $ncores --configfile data/${dataset}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete --config dataset=${dataset}
--- bash_preprocessing.sh
+++ bash_preprocessing.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-## the only expected argument is <dataset>
+#SBATCH --mail-user=thomas.brazier@univ-rennes1.fr
+#SBATCH --mail-type=all
+#SBATCH --mem=60GB
+#SBATCH --cpus-per-task=4
+#SBATCH --time=25-60:00:00
+#SBATCH --job-name=preprocess
 
-snakemake -s data_preprocessing.snake -p -j 16 --config dataset=${1} --configfile config.yaml --use-conda --use-singularity --nolock --rerun-incomplete
+# Load env
+. /local/env/envsnakemake-6.0.5.sh
+. /local/env/envsingularity-3.8.5.sh
+. /local/env/envconda.sh
+
+dataset=${1}
+ncores=16
+
+echo "Run pipeline"
+snakemake -s data_preprocessing.snake -p -j $ncores --configfile data/${dataset}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete --config dataset=${dataset}
