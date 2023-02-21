7eea69bb720decd1d7d205eea35e6f1ac3a841e1
tbrazier <brazier.thomas@gmail.com>
2023-02-21 09:21:36 +0100

    Pipeline to assess LDhat MCMC convergence

--- ldhat_MCMC.R
+++ ldhat_MCMC.R
@@ -5,58 +5,18 @@ args = commandArgs(trailingOnly=TRUE)
 
 # test if there is one argument: if not, return an error
 if (length(args) != 4) {
-  stop("<wdirpop>, <set>, <chromosome> and <bpen> must be passed in argument", call.=FALSE)
+  stop("<wdirpop>, <dataset>, <chromosome> and <bpen> must be passed in argument", call.=FALSE)
 }
-
 wdirpop = args[1]
-set = args[2]
+dataset = args[2]
 chromosome = args[3]
 bpen = args[4]
-
-cat("Loading MCMC sampling chain.\n")
-mcmc.file = paste(wdirpop, "/ldhat/", set, ".", chromosome, ".bpen", bpen, ".rates.txt.gz", sep = "")
-# mcmc.file = "data/Arabidopsis_thaliana_1001genomes.1.bpen15.rates.txt.gz"
-mcmc.chain = read.table(gzfile(mcmc.file), header = F, skip = 1)
-# One line per position
-# Each column is a MCMC iteration
-# Do the mean of each column (minus first one) and check if mean posterior is stable across iterations
-mean.posterior = data.frame(iteration = 1:(dim(mcmc.chain)[2] - 1),
-                            posone = as.numeric(mcmc.chain[1,-1]),
-                            pos1000 = as.numeric(mcmc.chain[1000,-1]),
-                            posterior = apply(mcmc.chain[,-1], 2, median),
-                            totalgenetic = apply(mcmc.chain[,-1], 2, sum))
-
-require(ggplot2)
-require(ggpubr)
-# Median of all positions
-p1 = ggplot(mean.posterior, aes(x = iteration, y = posterior)) +
-  geom_line() +
-  xlab("Sample") + ylab("Median posterior")
-p1b = ggplot(mean.posterior, aes(x = posterior)) +
-  geom_density() +
-  xlab("Median posterior")
-# One position
-p2 = ggplot(mean.posterior, aes(x = iteration, y = posone)) +
-  geom_line() +
-  xlab("Sample") + ylab("Posterior position one")
-p2b = ggplot(mean.posterior, aes(x = posone)) +
-  geom_density() +
-  xlab("Posterior position one")
-
-
-# ggplot(mean.posterior, aes(x = iteration, y = pos1000)) +
-#   geom_line()
-
-# Total genetic distance ~ sample (iteration)
-p3 = ggplot(mean.posterior, aes(x = iteration, y = totalgenetic)) +
-  geom_line() +
-  xlab("Sample") + ylab("Total genetic distance")
-p3b = ggplot(mean.posterior, aes(x = totalgenetic)) +
-  geom_density() +
-  xlab("Total genetic distance")
-
-
-p = ggarrange(p1, p1b, p2, p2b, p3, p3b, nrow = 3, ncol = 2)
-ggsave(paste(wdirpop, "/mcmc/", set, ".", chromosome, ".bpen", bpen, ".jpeg", sep = ""),
-       plot = p, device = jpeg(), width = 14, height = 12, dpi = 300)
-
+library(rmarkdown)
+library(vcfR)
+library(adegenet)
+library(poppr)
+rmarkdown::render('ldhat_MCMC.Rmd',
+                  params = list(wdirpop = wdirpop,
+                                dataset = dataset,
+                                chromosome = chromosome,
+                                bepn = bpen))
