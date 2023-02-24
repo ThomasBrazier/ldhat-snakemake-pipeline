f115019927318e482223b4ba7bad4a93195dd500
Thomas Brazier <thomas.brazier@univ-rennes1.fr>
2023-02-24 09:21:17 +0100

    Path to html output fixed

--- bash_mcmcchains.sh
+++ bash_mcmcchains.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mail-user=thomas.brazier@univ-rennes1.fr
 #SBATCH --mail-type=all
-#SBATCH --mem=60GB
+#SBATCH --mem=180GB
 #SBATCH --cpus-per-task=4
 #SBATCH --time=25-60:00:00
 #SBATCH --job-name=ldhatMCMC
