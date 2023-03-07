0af3ada0c928e2a2b8cbb3409f7f12fb3990e142
Thomas Brazier <thomas.brazier@univ-rennes1.fr>
2023-03-07 12:46:22 +0100

    Minor changes

--- bash_mcmcchains.sh
+++ bash_mcmcchains.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-user=thomas.brazier@univ-rennes1.fr
 #SBATCH --mail-type=all
 #SBATCH --mem=200GB
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=10
 #SBATCH --time=25-60:00:00
 #SBATCH --job-name=ldhatMCMC
 
@@ -11,7 +11,7 @@
 . /local/env/envconda.sh
 
 dataset=${1}
-ncores=1
+ncores=10
 
 echo "Run pipeline"
 snakemake -s data_mcmcchains.snake -p -j $ncores --configfile data/${dataset}/config.yaml --use-conda --use-singularity --nolock --rerun-incomplete --config dataset=${dataset}
