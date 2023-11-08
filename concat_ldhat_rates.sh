111f4c4e2a0b55534bc5279178031a304b787d58
tbrazier <brazier.thomas@gmail.com>
2023-11-08 17:01:13 +0100

    Concat LDhat rates in columsn instead of lines

--- concat_ldhat_rates.sh
+++ concat_ldhat_rates.sh
@@ -16,9 +16,13 @@ echo $n_batch
 for f in $(seq 1 $(( $n_batch )))
 do
   echo "Processing bpen${bpen}.batch_${f}.rates.txt.gz file..."
-  zcat bpen${bpen}.batch_${f}.rates.txt | tail -n +2 >> bpen${bpen}.rates_noheader.txt
+  zcat bpen${bpen}.batch_${f}.rates.txt | tail -n +2 > tmp.txt
+  paste bpen${bpen}.rates_noheader.txt tmp.txt
 done
-n_snps=$(cat bpen${bpen}.rates_noheader.txt | wc -l)
+# n_snps=$(cat bpen${bpen}.rates_noheader.txt | wc -l)
+n_snps=$(awk '{print NF}' bpen${bpen}.rates_noheader.txt | sort -nu | tail -n 1)
+
+rm tmp.txt
 
 echo "$n_snps	$n_snps" > bpen${bpen}.rates.txt
 cat bpen${bpen}.rates_noheader.txt >> bpen${bpen}.rates.txt
