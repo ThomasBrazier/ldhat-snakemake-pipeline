e37897c73fd0d0145490558a8ef837ada5bedc4d
tbrazier <brazier.thomas@gmail.com>
2023-11-13 11:49:01 +0100

    Test the bugfix for concat LDhat rates when large sample option

--- concat_ldhat_rates.sh
+++ concat_ldhat_rates.sh
@@ -13,15 +13,20 @@ test -f bpen${bpen}.rates_noheader.txt && rm bpen${bpen}.rates_noheader.txt
 n_batch=$(ls | grep "bpen${bpen}.batch_" | grep ".rates.txt" | wc -l)
 echo $n_batch
 
+rm bpen${bpen}.rates_noheader.txt
+touch bpen${bpen}.rates_noheader.txt
+
 for f in $(seq 1 $(( $n_batch )))
 do
   echo "Processing bpen${bpen}.batch_${f}.rates.txt.gz file..."
-  zcat bpen${bpen}.batch_${f}.rates.txt | tail -n +2 > tmp.txt
-  paste bpen${bpen}.rates_noheader.txt tmp.txt
+  zcat bpen${bpen}.batch_${f}.rates.txt.gz | tail -n +2 > tmp.txt
+  paste bpen${bpen}.rates_noheader.txt tmp.txt > tmp2
+  cat tmp2 > bpen${bpen}.rates_noheader.txt
 done
 # n_snps=$(cat bpen${bpen}.rates_noheader.txt | wc -l)
 n_snps=$(awk '{print NF}' bpen${bpen}.rates_noheader.txt | sort -nu | tail -n 1)
 
+rm tmp2
 rm tmp.txt
 
 echo "$n_snps	$n_snps" > bpen${bpen}.rates.txt
