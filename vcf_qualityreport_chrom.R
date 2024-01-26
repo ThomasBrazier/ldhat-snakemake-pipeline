ca72385d537279f6f2d82f89efe458722c4fedfa
tbrazier <brazier.thomas@gmail.com>
2024-01-26 13:55:53 +0100

    Output dir in VCF quality Rmd

--- vcf_qualityreport_chrom.R
+++ vcf_qualityreport_chrom.R
@@ -7,4 +7,6 @@ library(vcfR)
 library(adegenet)
 library(poppr)
 rmarkdown::render('vcf_qualityreport_chrom.Rmd',
+                  output_file = paste0(dataset, ".", chromosome, ".bpen", bpen, ".quality.html"),
+                  output_dir = paste0(wdirpop),
                   params = list(dat = vcf_file, chrom = chrom))
