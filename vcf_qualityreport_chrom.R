759d4afd5d0dcaa8fea3b5d7b6b927fe5e0e1c6e
tbrazier <brazier.thomas@gmail.com>
2024-01-26 14:25:55 +0100

    Minor changes

--- vcf_qualityreport_chrom.R
+++ vcf_qualityreport_chrom.R
@@ -3,12 +3,11 @@ args = commandArgs(trailingOnly = TRUE)
 dataset = args[1]
 chrom = args[2]
 wdirpop = args[3]
-bpen = args[4]
 library(rmarkdown)
 library(vcfR)
 library(adegenet)
 library(poppr)
 rmarkdown::render('vcf_qualityreport_chrom.Rmd',
-                  output_file = paste0(dataset, ".", chromosome, ".bpen", bpen, ".quality.html"),
+                  output_file = paste0(dataset, ".", chrom, ".quality.html"),
                   output_dir = paste0(wdirpop),
                   params = list(dat = dataset, chrom = chrom))
