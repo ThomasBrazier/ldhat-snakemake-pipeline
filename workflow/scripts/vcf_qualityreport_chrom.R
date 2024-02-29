c4af5f7cbcbd4f6c5b3b332d6d302023c20cc158
Thomas Brazier <brazier.thomas@gmail.com>
2024-02-29 09:50:53 +0100

    Relative path

--- workflow/scripts/vcf_qualityreport_chrom.R
+++ workflow/scripts/vcf_qualityreport_chrom.R
@@ -8,7 +8,7 @@ library(rmarkdown)
 library(vcfR)
 library(adegenet)
 library(poppr)
-rmarkdown::render('vcf_qualityreport_chrom.Rmd',
+rmarkdown::render('workflow/scripts/vcf_qualityreport_chrom.Rmd',
                   output_file = paste0(dataset, ".", chrom, ".bpen", bpen, ".quality.html"),
                   output_dir = paste0(wdirpop),
                   params = list(dat = dataset, chrom = chrom))
