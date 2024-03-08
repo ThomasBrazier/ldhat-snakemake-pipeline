f5594110533eab34da8e13f3585cf4f5ac9fffe1
Thomas Brazier <brazier.thomas@gmail.com>
2024-03-08 11:10:05 +0100

    Fix paths in LDhat Rmd reports

--- workflow/scripts/vcf_qualityreport_chrom.R
+++ workflow/scripts/vcf_qualityreport_chrom.R
@@ -4,6 +4,7 @@ dataset = args[1]
 chrom = args[2]
 wdirpop = as.character(args[3])
 bpen = as.character(args[4])
+
 library(rmarkdown)
 library(vcfR)
 library(adegenet)
@@ -11,4 +12,4 @@ library(poppr)
 rmarkdown::render('workflow/scripts/vcf_qualityreport_chrom.Rmd',
                   output_file = paste0(dataset, ".", chrom, ".bpen", bpen, ".quality.html"),
                   output_dir = paste0(wdirpop),
-                  params = list(dat = dataset, chrom = chrom))
+                  params = list(dat = dataset, chrom = chrom, wdirpop = wdirpop))
